4678f070e167e8aceacb229d6bd8c922c139c7b9
Mateusz Cywisnki <cywinskimateusz@gmail.com>
2023-01-19 12:24:51 +0100

    Fix of page loading
    Add config option to disable security check on page load

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 5000,
   pageLoadTimeout: 5000,
+  chromeWebSecurity: false,
   e2e: {
-    baseUrl: `https://www.saucedemo.com/`,
+    baseUrl: `https://saucedemo.com/`,
     defaultCommandTimeout: 5000,
     viewportHeight : 1080,
     viewportWidth : 1920,
     retries: {
-      "runMode" : 1,
-      "openMode" : 1
-    },
-    setupNodeEvents(on, config) {
-    },
-  },
+      "runMode" : 0,
+      "openMode" : 0
+    }
+  }
 });
